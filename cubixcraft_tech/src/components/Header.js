7aae0f6822e7858a0dc08efed16238e1180f214c
alok003 <83292268+alok003@users.noreply.github.com>
2023-12-21 19:34:34 +0530

    correcting all links and target pages

--- cubixcraft_tech/src/components/Header.js
+++ cubixcraft_tech/src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
     <div>
         <h1>ALOK KUMAR</h1>
         <ul>
-            <li><Link to='/'>Home</Link></li>
+            <li><Link to='https://alok003.github.io/mGY3R2Vbv0d25UCB/'>Home</Link></li>
             <li><Link to='/BMIcalc'>BMI Calculator</Link></li>
             <li><Link to='https://drive.google.com/file/d/1zcL6dkzeHQzoNiEzHn76uT9z1extlUg3/view?usp=drive_link'>Resume</Link></li>
         </ul>
